530af628d4ae23d8641c0bfb91926ba0083cbbe9
Yury Prokashev <yury.prokashev@gmail.com>
2017-01-27 07:32:49 +0300

    update

--- src/myConfig.js
+++ src/myConfig.js
@@ -1,3 +1,7 @@
 /**
  * Created by py on 27/01/2017.
  */
+'use strict';
+module.exports.configCtrlFactory = require('./configCtrlFactory');
+module.exports.configServiceFactory = require('./configServiceFactory');
+module.exports.configObjectFactory = require('./configObjectFactory');
